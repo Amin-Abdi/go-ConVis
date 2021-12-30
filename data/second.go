637397ebf7138a5202c90927760950bfd51c9b6f
Amin <malmaaz78@gmail.com>
2021-12-30 16:11:47 +0000

    Added representation

--- data/second.go
+++ data/second.go
@@ -2,22 +2,12 @@ package data
 
 func main() {
 	first := make(chan int)
-	third := make(chan int)
 
-	go sender(first, "Hasicas", third)
+	go sender(first)
 
 	<-first
 }
 
-func sender(c chan int, k string, a chan int) {
-	second := make(chan string)
-	go pong(second)
-
+func sender(c chan int) {
 	c <- 99
-	second <- "Hello World"
-
-}
-
-func pong(b chan string) {
-	<-b
 }
